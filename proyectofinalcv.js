0485fc6351ba6195c5d382238f25e29760e4abb8
GASTON2808 <95896682+GASTON2808@users.noreply.github.com>
2023-01-31 21:33:34 -0300

    Add files via upload

---
+++ proyectofinalcv.js
@@ -0,0 +1,4 @@
+document.getElementById("mostrarcontacto").addEventListener("click", function(){
+    console.log("capturamos el evento click y mostramos el contacto")
+    document.getElementById("contacto").innerHTML = "Celular: 11235813 / Correo Electrónico: hortailodelasmilcasas@email.com / Dirección: Fibonacci 1123, Ciudad Fractal";
+});
